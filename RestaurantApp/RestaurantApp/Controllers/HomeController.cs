c703c0b9f0b4e4c230bba130e73a9c5c18dc9f87
SumanthChamakura <112259096+SumanthChamakura@users.noreply.github.com>
2022-09-28 09:26:15 +0530

    new_commit919

---
+++ RestaurantApp/RestaurantApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using RestaurantApp.Models;
+using RestaurantApp.Repositories;
+using RestaurantApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RestaurantDBEntities objRestaurantDBEntities;
+        public HomeController()
+        {
+            objRestaurantDBEntities = new RestaurantDBEntities();
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            CustomerRepository objCustomerRepository =new CustomerRepository();
+            ItemRepository objitemRepository =new ItemRepository();
+            PaymentTypeRepository objpaymentTypeRepository =new PaymentTypeRepository();
+
+            var objMultipleModels = new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>
+                (objCustomerRepository.GetAllCustomers(), objitemRepository.GetAllItems(), objpaymentTypeRepository.GetAllPaymetType());
+
+            return View(objMultipleModels);
+        }
+        [HttpGet]
+        public JsonResult getItemUnitPrice(int itemId)
+        {
+            decimal UnitPrice = objRestaurantDBEntities.Items.Single(model=>model.ItemId == itemId).ItemPrice;
+            return Json(UnitPrice, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Index(OrderViewModel objOrderViewModel)
+        {
+            OrderRepository objOrderRepository = new OrderRepository();
+            objOrderRepository.AddOrder(objOrderViewModel);
+            return Json("Your order has been Successfully place.", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
